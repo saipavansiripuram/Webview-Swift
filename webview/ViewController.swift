fd84eefb42f62d64566f5c7c34fefd20d704e58f
saipavan <saipavansiripuram5@gmail.com>
2022-11-22 20:27:57 +0530

    First commit

--- webview/ViewController.swift
+++ webview/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let url = URL(string : "https://google.com")
+        web.load(URLRequest(url: url!))
     }
 
-
+    @IBOutlet weak var web: WKWebView!
+    
+    @IBAction func back(
+        _ sender: Any) {
+            web.goBack()
+    }
+    
+    @IBAction func refresh(_ sender: Any) {
+        web.reload()
+    }
+    
+    
+    @IBAction func forward(_ sender: Any) {
+        web.goForward()
+    }
+    
+    
+    
+    
 }
 
